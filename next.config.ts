c0171d858faa3865d869e953b022ee3f87566231
Albert Lim <alim@cardiweb.com>
2025-02-18 00:37:58 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/cathodle/" : "",
-  basePath: isProd ? "/cathodle" : "",
+  basePath: "/cathodle",
   output: "export",
 };
 
